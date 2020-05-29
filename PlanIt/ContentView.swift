2844dce457254e2b24256ef06ce2f6e6ec26c779
Helen <HelenWang170@gmail.com>
2020-05-29 14:04:32 -0500

    Initial Commit

---
+++ PlanIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlanIt
+//
+//  Created by Helen Wang on 5/29/20.
+//  Copyright © 2020 Helen Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
